d6ecd253405334626ce4556fe84fba6159ba62fe
guna3005 <pranithreddyguna01@gmail.com>
2024-02-22 16:28:09 -0500

    rendering chart  data through server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+// Budget API
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+const bud = require("./public/budget.json");
+
+app.use("/", express.static("public"));
+
+app.get("/budget", (req, res) => {
+  res.json(bud);
+});
+
+app.listen(port, () => {
+  console.log(`API served at http://localhost:${port}`);
+});
